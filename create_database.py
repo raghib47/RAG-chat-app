c72b2a27b4a498f04c6c3c9ead7f134c51b52fcf
Raghib Shahriyar <raghibn35@gmail.com>
2026-07-26 01:54:59 +0600

    Add files via upload

---
+++ create_database.py
@@ -0,0 +1,27 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_mistralai import MistralAIEmbeddings
+from langchain_community.vectorstores import Chroma
+from dotenv import load_dotenv
+
+load_dotenv()
+
+data = PyPDFLoader(r"C:\Users\Raghib\Desktop\RAG Project\document loaders\DEEP_LEARNING.pdf")
+
+
+docs = data.load()
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 1000,
+    chunk_overlap = 200
+)
+
+chunks = splitter.split_documents(docs)
+
+embedding_model = MistralAIEmbeddings()
+
+vectorstore = Chroma.from_documents(
+    documents= chunks,
+    embedding=embedding_model,
+    persist_directory="chroma_db"
+)
